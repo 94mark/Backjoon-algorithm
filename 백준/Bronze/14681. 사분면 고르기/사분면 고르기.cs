5e3f6c391d3c268a6fe6366d788a1e152e573a5e
Seongju <seongju0007@gmail.com>
2022-08-04 23:09:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 92 ms, Memory: 5720 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > 0)
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("1");
+                }
+                else if(y < 0)
+                {
+                    Console.WriteLine("4");
+                }
+            }
+
+            else if (x < 0)
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("2");
+                }
+                else if (y < 0)
+                    {
+                    Console.WriteLine("3");
+                }
+            }
+        }
+    }
+}
