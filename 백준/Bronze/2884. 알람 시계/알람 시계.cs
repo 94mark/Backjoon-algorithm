a31323b7e47fbcbd413a0604cf2e23461b382b56
Seongju <seongju0007@gmail.com>
2022-08-05 00:21:35 +0900

    [Bronze III] Title: 알람 시계, Time: 64 ms, Memory: 5488 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cs
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cs
@@ -7,18 +7,46 @@ namespace Backjoon
         static void Main(string[] args)
         {
             string[] time = Console.ReadLine().Split(" ");
-
             int h = int.Parse(time[0]);
             int m = int.Parse(time[1]);
-
-            if (m < 45)
+            
+            /*if(h != 0)
+            {               
+               if(m < 45)
+               {                   
+                    h = h - 1;
+                    m = m + 15;
+               }
+               else if(m >= 45 && m <= 60)
+               {                   
+                    h = h;
+                    m = m - 45; 
+               }
+            }
+            else if(h < 0)
+            {
+                if(m < 45)
+               {                   
+                    h = 23;
+                    m = m + 15;
+               }
+               else if(m >= 45 && m <= 60)
+               {                   
+                    h = 23;
+                    m = m - 45; 
+               }
+            }*/
+            
+            if(m < 45)
             {
                 m += 60;
                 h--;
-                if (h < 0)
-                    h = 23;                
+                if(h < 0)
+                    h = 23;
             }
-            Console.WriteLine("{0} {1}",h,m-45);
+                
+            
+            Console.WriteLine("{0} {1}", h, m-45);                
         }
     }
 }
