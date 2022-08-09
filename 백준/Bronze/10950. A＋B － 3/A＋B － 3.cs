183d6dd4850e72dbdd2c9fe7194739670cd173aa
Seongju <seongju0007@gmail.com>
2022-08-09 23:04:36 +0900

    [Bronze V] Title: A+B - 3, Time: 80 ms, Memory: 5636 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.cs
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cs
@@ -1,20 +1,20 @@
 using System;
 
-namespace backjoon
+namespace Backjoon
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int t = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < t; i++)
+            int num = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i < num; i++)
             {
-                string[] num = Console.ReadLine().Split();
-                int numA = int.Parse(num[0]);
-                int numB = int.Parse(num[1]);
-
-                Console.WriteLine(numA + numB);
+                string[] str = Console.ReadLine().Split();
+                int a = int.Parse(str[0]);
+                int b = int.Parse(str[1]);
+                
+                Console.WriteLine(a + b);
             }
         }
     }
