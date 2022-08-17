086c5df562efb0e6e74d5e726cc2e4a7a4402ebd
Seongju <seongju0007@gmail.com>
2022-08-17 23:32:16 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 460 ms, Memory: 6588 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
